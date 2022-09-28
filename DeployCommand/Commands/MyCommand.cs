7a4347da0a75d2889d646137e517c523c6398443
rohansharma <rohansharma@LAPTOP-5791J53O>
2022-09-28 12:16:39 +0530

    Add project files.

---
+++ DeployCommand/Commands/MyCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace DeployCommand
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+
+            Process.Start("cmd", "/k PowerShell.exe -command \"C:\\Users\\rohansharma\\source\\repos\\sample.ps1\"");
+            await VS.MessageBox.ShowWarningAsync("DeployCommand", "Button clicked");
+        }
+    }
+}
